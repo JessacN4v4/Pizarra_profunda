19d6cb3ba5d720928fbed87b7a0d1933b2730f77
JessacN4v4 <a01713439@tec.mx>
2025-06-12 08:38:43 -0600

    main.cpp para correr el codigo completo

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include "maleta_base.h"
+#include "maleta.h" 
+#include "equipaje_especial.h"
+#include "avion.h"
+#include "sistema_maletas.h"
+
+int main() {
+    sistema_maletas sistema;
+    sistema.mostrar_menu();
+    return 0;
+}
